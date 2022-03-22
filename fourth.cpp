7244e4b0742533e5408892bfe1f224026c5e8864
Santosh payasi <santoshpayasi9@gmail.com>
2022-03-23 01:50:40 +0530

    fourth cpp

---
+++ fourth.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int l,b,r,a,p,c;
+    float pai=3.14;
+    cout<<"enter the value of length and breadth :"<<endl;
+    cin>>l>>b;
+    cout<<"enter the radius of the circle :";
+    cin>>r;
+    cout<<"perimeter  of the rectangle is :"<<2*(l*b);
+    cout<<"area of the circle is :"<<pai*r*r;
+    
+}
